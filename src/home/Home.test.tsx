c947d798d84658c6dee906fc65da1eb483c509a1
Armin Plohl <armin.plohl@myworld.com>
2022-04-01 01:18:14 +0200

    Removed unused files

--- src/home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from '../endpoint/Home';
-
-test('renders title', () => {
-  render(<Home />);
-  const linkElement = screen.get('Recipes');
-  expect(linkElement).toBeInTheDocument();
-});
